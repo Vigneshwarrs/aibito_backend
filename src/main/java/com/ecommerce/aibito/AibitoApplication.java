95eaa98be9391a766845cd001c9bd7ac98aea559
Vigneshwarrs <vigneshwarrs@outlook.com>
2025-02-28 18:20:57 +0530

    basic setup

---
+++ src/main/java/com/ecommerce/aibito/AibitoApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.aibito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AibitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AibitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/aibito/AibitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.aibito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AibitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
